5735fac42f881fbd65d1299ad1843b81f1a2e58e
Denis Chernykh <education03@mail.ru>
2026-05-30 18:59:03 +0500

    fix: configure production database and admin URL for CodeRed

--- cycling_site/settings/prod.py
+++ cycling_site/settings/prod.py
@@ -7,6 +7,22 @@ DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY") or os.environ["RANDOM_SECRET_KEY"]
 ALLOWED_HOSTS = [os.environ["VIRTUAL_HOST"]]
 
+# CodeRed Cloud provides PostgreSQL connection details via these env vars
+# (not via DATABASE_URL). See https://www.codered.cloud/docs/django/environment/
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ.get("DB_PORT", "5432"),
+        "CONN_MAX_AGE": 600,
+    }
+}
+
+WAGTAILADMIN_BASE_URL = f"https://{os.environ['VIRTUAL_HOST']}"
+
 STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 try:
