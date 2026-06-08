12e02c7ec7a9bef5f8a9a57066c3b7f606f55323
Denis Chernykh <education03@mail.ru>
2026-06-08 12:36:12 +0500

    fix: disable unicode slugs in Wagtail to prevent Cyrillic URLs

--- cycling_site/settings/base.py
+++ cycling_site/settings/base.py
@@ -252,6 +252,7 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 10_000
 # Wagtail settings
 
 WAGTAIL_SITE_NAME = "cycling_site"
+WAGTAIL_ALLOW_UNICODE_SLUGS = False
 
 # Search
 # https://docs.wagtail.org/en/stable/topics/search/backends.html
