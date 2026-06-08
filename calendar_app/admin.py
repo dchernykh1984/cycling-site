aa9c7d7cabc3604a6f3f2cb0c77115ca3d09b5ac
Denis Chernykh <education03@mail.ru>
2026-06-08 19:51:05 +0500

    fix: admin textarea for comment body, allow clearing preferred_language to Auto

--- calendar_app/admin.py
+++ calendar_app/admin.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from typing import ClassVar
+
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea
 
 from .models import Competition, CompetitionComment
 
@@ -19,3 +23,6 @@ class CompetitionCommentAdmin(admin.ModelAdmin):
     list_filter = ("competition",)
     readonly_fields = ("created_at",)
     search_fields = ("body", "author__email")
+    formfield_overrides: ClassVar[dict] = {
+        models.CharField: {"widget": Textarea(attrs={"rows": 4, "cols": 80})},
+    }
