9738fb062b43387b786133374d9da07c4b3ba16a
Denis Chernykh <education03@mail.ru>
2026-06-11 15:07:27 +0500

    feat: enable mandatory email verification and assign GUEST role until confirmed

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -5,7 +5,7 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 class AccountAdapter(DefaultAccountAdapter):
     def save_user(self, request, user, form, commit=True):
         user = super().save_user(request, user, form, commit=False)
-        user.role = user.Role.PARTICIPANT
+        user.role = user.Role.GUEST
         if commit:
             user.save()
         return user
