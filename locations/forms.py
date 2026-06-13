60e2e4c73c264637358fd84bedf8c7dddf457349
Denis Chernykh <education03@mail.ru>
2026-06-13 20:23:32 +0500

    feat: replace parent dropdown with cascading country/region/city and translate form labels

--- locations/forms.py
+++ locations/forms.py
@@ -8,22 +8,21 @@ class LocationForm(forms.Form):
     name_ru = forms.CharField(max_length=255, label=_("Name (RU)"))
     name_kk = forms.CharField(max_length=255, label=_("Name (KK)"), required=False)
     name_en = forms.CharField(max_length=255, label=_("Name (EN)"), required=False)
-    parent = forms.ModelChoiceField(
+    city = forms.ModelChoiceField(
         queryset=Location.objects.none(),
-        required=False,
-        label=_("Parent location"),
-        empty_label=_("top level"),
+        required=True,
+        label=_("City"),
+        empty_label=None,
+        widget=forms.HiddenInput(),
     )
     lat = forms.DecimalField(max_digits=9, decimal_places=6, required=False, label=_("Latitude"))
     lng = forms.DecimalField(max_digits=9, decimal_places=6, required=False, label=_("Longitude"))
     is_hidden = forms.BooleanField(required=False, label=_("Hidden"))
 
-    def __init__(self, *args, exclude_pk=None, **kwargs):
+    def __init__(self, *args, exclude_pk=None, location_depth=4, **kwargs):
         super().__init__(*args, **kwargs)
-        qs = Location.objects.filter(is_deleted=False).order_by("path")
+        qs = Location.objects.filter(is_deleted=False, depth=3).order_by("path")
         if exclude_pk is not None:
             qs = qs.exclude(pk=exclude_pk)
-        self.fields["parent"].queryset = qs
-        self.fields["parent"].label_from_instance = lambda obj: (
-            "--" * (obj.depth - 1) + " " + (obj.name or f"#{obj.pk}") if obj.depth > 1 else (obj.name or f"#{obj.pk}")
-        )
+        self.fields["city"].queryset = qs
+        self.fields["city"].required = location_depth >= 4
